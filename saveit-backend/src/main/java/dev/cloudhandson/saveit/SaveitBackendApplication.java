9fc923ce556592abe23f8fadfa2158e636945fd5
Thakur13 <asthakur185@gmail.com>
2023-05-21 20:42:48 +0530

    Added Spring Boot backend

---
+++ saveit-backend/src/main/java/dev/cloudhandson/saveit/SaveitBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.saveit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveitBackendApplication.class, args);
+    }
+
+}
---
+++ saveit-backend/src/test/java/dev/cloudhandson/saveit/SaveitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloudhandson.saveit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
